c79e4bb618835c414c6a60b47143567d42a0f97b
Jackbennett <jbennett@birkdalehigh.co.uk>
2018-01-17 16:23:22 +0000

    new function Get-SchoolUser, replaced New-CAuser with New-CADirectory
    CA users's are no longer separate so as such do not need creating.
    instead the fucntion created their new work folder that is either
    separatly mapped to them or replaces their logon drive at certain times.
    
    Get-SchoolUser is new to make it easier checking users AD properties and
    finding users in the first place.

--- User.ps1
+++ User.ps1
@@ -77,35 +77,47 @@
     }
 }
 
-function New-CAUser{
+function New-CADirectory{
     <#
     .SYNOPSIS
-        Create a subject specific user account
+        Create a folder specific to user account by intake year and subject
     .DESCRIPTION
-        Create a user account for controlled assessment work.
-        By Making the account subject specific admins can delegate managing access and logon times as there's no other subjects use of the account to impact.
+        Controlled assessments have their own work area to easy marking, specify access controls.
+
+        This folder can be mapped as a separate drive for students to use by mebership of AccessCAShared.
+        This group membership allows the policy "Add - Drive J - CA Work" to map the drive '\\bhs-fs01\ca'.
+        Access based enumeration takes care of simplify the folders users will see in this drive.
+
+        Alternatively they can be used as the home directory when the user is a member of DisableHomeFolder.
+        The policy "Set - Drive N - CA Home Path" can target a drive map to this path
+    .INPUTS
+        Microsoft.ActiveDirectory.Management.ADUser
+    .OUTPUTS
+        System.IO.DirectoryInfo
     .EXAMPLE
-        get-aduser example | new-causer -SubjectCode Hi -intake 2016
-        Explanation of what the example does
+        Get-ADGroupMember 10c_im1 | New-CADirectory -SubjectName iMedia -intake 2014
+        Will create in this case 26 folders of the usernames from the 10c_Im1 gorup membership at
+        the path \\bhs-fs01\CA\Intake 2014\iMedia\%username% with fullControl folder permissions by each username.
     .NOTES
-        Development improvements:
-        - Could put the users in a better subject-specific OU Path. Currently manually done.
-        - Could guess the intake year from the OU path of the sipplied identity
+        TODO: Validate subject name against Get-ClassProperty for consistancy.
     #>
     [cmdletbinding()]
     Param(
         [Parameter(Mandatory=$true,
+                   Position=0,
+                   ValueFromPipeline=$true,
                    ValueFromPipelineByPropertyName=$true)]
         [Microsoft.ActiveDirectory.Management.ADUser]$Identity,
 
+        # Full subject name e.g. 'Computer Science'
         [Parameter(Mandatory=$true,
+                   Position=2,
                    ValueFromPipelineByPropertyName=$true)]
-        [ValidateSet('Ar','Bi','Bt','Bu','Ch','Cs','Dr','En','Fo','Fr','Gg','Hi','It','Ma','Mu','Pe','Ph','Re','Rm','Sc','Sp','Te','Xl')]
-        [string]$SubjectCode,
+        [string]$SubjectName,
 
+        # Intake Year
         [Parameter(Mandatory=$true,
-                   Position=0,
-                   ValueFromPipeline=$true,
+                   Position=1,
                    ValueFromPipelineByPropertyName=$true)]
         [ValidateScript({
             $year = (get-date).year
@@ -117,54 +129,54 @@ function New-CAUser{
         })]
         [string]$intake
     )
-    Process{
+    Begin {
         [string]$year = $intake
-        $username = $Identity.SamAccountName.Insert(0, $SubjectCode)
+        [string]$PathRoot = "\\bhs-fs01\CA\Intake $Year"
+        [string]$PathSubject = join-path $PathRoot $SubjectName
 
-        if($username.length -gt 20){
-            Throw "$username is over 20 characters"
+        # Validate intake path exists or to be created
+        if(-not (Test-Path $PathRoot)){
+            Write-Warning "Mising $PathRoot. Ensure AccessCAShared has read/execute to access this folder only"
+            $invalidRoot = $true
+        }
+        if(-not (Test-Path $PathSubject)){
+            Write-Warning "Mising $PathSubject. Ensure desired CA_Intake_Subject group has read/execute to access this folder only"
+            $invalidsubject = $true
+        }
+        if($invalidRoot -and -$invalidSubject){
+            Throw "Please Fix path errors to continue."
         }
 
-        $identity | new-aduser
-            -name $username `
-            -Path "OU=$year,OU=Controlled Assessment,OU=Users,OU=BHS,DC=BHS,DC=INTERNAL" `
-            -ProfilePath "\\bhs-fs01\profiles$\Students\$year Students CA" `
-            -HomeDirectory "\\bhs-fs01\ca\Intake $year\$username" `
-            -HomeDrive 'N:' `
-            -ScriptPath 'kix32 Students.kix' `
-            -UserPrincipalName "$username@BHS.INTERNAL" `
-            -AccountPassword (ConvertTo-SecureString -AsPlainText -Force "password") `
-            -ChangePasswordAtLogon $true `
-            -Enabled $true
+        $Propagation = [System.Security.AccessControl.PropagationFlags]::None
+        $Type =[System.Security.AccessControl.AccessControlType]::Allow
     }
-}
-
-function New-CAClassMember {
-    <#
-    .SYNOPSIS
-        Add user to CA Class Group
-    .DESCRIPTION
-        Long description
-    .NOTES
-        General notes
-    #>
-    Param(
-        # Class code of the ad group to get e.g. 11D_hi1
-        $ClassCode
-        , # User intake year
-        [Parameter(Mandatory=$true)]
-        [ValidateScript({
-            $year = (get-date).year
-            if( ($PSItem -le $year) -and ($PSItem -ge $year-5) ){
-                return $true
-            } else {
-                Throw "$psitem is not an active intake year."
+    Process {
+        # Create user folder for each username, assigning onwership permission.
+        $Identity | foreach-Object {
+            # TODO: Handle an existing directory with a warning
+            try {
+                $Directory = new-item -ItemType Directory -Path (Join-Path $PathSubject $psitem.samAccountName) -ErrorAction stop
+            } catch [System.IO.IOException] {
+                Write-Warning $psitem.exception.message
+                return
+                # skip the loop for this user.
+            } catch {
+                throw $psitem
+                # Break with the full error for troubleshooting.
             }
-        })]
-        [string]$Intake
-    )
-    $class = Get-ClassProperty $ClassCode -ErrorAction Stop
-    Get-ADGroupMember $class.Code | get-aduser | new-causer -SubjectCode $class.id -intake $Intake
+            $item = get-acl $Directory
+
+            $Principal = New-Object System.Security.Principal.NTAccount($psitem.samAccountName)
+            $Entry = New-Object System.Security.AccessControl.FileSystemAccessRule($Principal, 'FullControl', 'ContainerInherit,ObjectInherit', $Propagation, $Type)
+            $item.AddAccessRule($Entry)
+            Set-ACL $item.path $item
+            Get-Item $item.path
+        }
+
+    }
+    end {
+        # TODO: Test each folder exists and permissions for user applied
+    }
 }
 
 function Reset-ADPassword{
@@ -294,4 +306,106 @@ function Test-HomeDirectory{
     #     (test-path $_.homeDirectory) -eq $false
     # }
 
+}
+
+function Get-SchoolUser {
+    <#
+    .SYNOPSIS
+        Get AD Users with properties useful to school administration
+    .DESCRIPTION
+        As well as providing a shortcut to useful AD properties it can also easily search for users by common first and last names.
+
+        Output can include disabled user accounts so be mindful with your results.
+
+        Note that by default the exact username will be queried. Specify named parameters to search by either first and/or last names.
+        Names can have wild cards appened
+    .EXAMPLE
+        PS C:\> get-schooluser student
+        Gets the specific username "student" details
+
+        GivenName         : Student
+        Surname           : Orgname
+        SamAccountName    : student
+        DistinguishedName : CN=Student Orgname,OU=Test,OU=ORG,DC=ORG,DC=INTERNAL
+        Enabled           : True
+        HomeDirectory     : \\org-server01\files\students\Test\student
+        EmployeeNumber    : 000000
+        EmailAddress      : student@example.com
+    .EXAMPLE
+        PS C:\> Get-SchoolUser -Givenname jack | measure
+        Measuring the number of users whose first name is "jack".
+
+        Count    : 87
+    .EXAMPLE
+        PS C:\> Get-SchoolUser -Givenname jack | where enabled | measure
+        Measuring the number of enabled accounts with a givenname of "jack".
+
+        Count    : 21
+    .EXAMPLE
+        PS C:\> Get-SchoolUser ben | measure
+        This assumes the exact user account name is asked for.
+
+        Get-ADUser : Cannot find an object with identity: 'ben'
+    .EXAMPLE
+        PS C:\> Get-SchoolUser -forename ben | measure
+        This assumes the exact user account name is asked for.
+        ben* would actully return 19 users to account for benjamin etc.
+
+        Count    : 5
+    .OUTPUTS
+        Microsoft.ActiveDirectory.Management.ADUser
+    .NOTES
+        General notes
+    #>
+    [CmdletBinding(DefaultParameterSetName='Specific')]
+    Param(
+        # Search by first name
+        [Parameter(Position=0, ParameterSetName='Search')]
+        [alias('Forename','Firstname')]
+        [string]
+        $Givenname = '*'
+
+        , # Search by surname
+        [Parameter(Position=1, ParameterSetName='Search')]
+        [alias('Lastname')]
+        [string]
+        $Surname = '*'
+
+        , # User Logon Name
+        [Parameter(Position=0, Mandatory, ParameterSetName='Specific', ValueFromPipelineByPropertyName, ValueFromPipeline)]
+        [string[]]
+        $SamAccountName
+    )
+    Begin{
+        $props = @(
+            'HomeDirectory',
+            'EmployeeNumber',
+            'EmailAddress'
+        )
+        $outputFields = @(
+            'GivenName',
+            'Surname',
+            'SamAccountName',
+            'DistinguishedName',
+            'Enabled'
+        ) + $props
+    }
+    Process{
+        switch($PsCmdlet.ParameterSetName){
+            'Specific' {
+                $SamAccountName | Get-ADUser -properties $props |
+                    Select-Object -Property $outputFields |
+                    Write-Output
+                return
+            }
+            'Search' {
+                $queryString = "(&(objectClass=user)(sn=$Surname)(givenname=$givenname))"
+                Write-Verbose "LDAP Query String: $queryString"
+                Get-ADUser -LDAPfilter $queryString -properties $props |
+                    Select-Object -Property $outputFields |
+                    Write-Output
+            }
+        }
+
+    }
 }
