324e7e2ee72493ca658cf7b7c36ed3328305597a
Jackbennett <jbennett@birkdalehigh.co.uk>
2018-10-16 10:58:28 +0100

    initial work on how group membership management will be implemented

---
+++ group.ps1
@@ -0,0 +1,29 @@
+function Test-Group {
+    <#
+    .Synopsis
+        Test that the groups to be enforced actually exist
+    #>
+    Param(
+        $Identity
+    )
+}
+
+function Test-GroupMembership {
+    <#
+    .Synopsis
+        Return whether users are in the correct groups as defined.
+    #>
+    [CmdletBinding()]
+    param (
+        
+    )
+    
+    begin {
+    }
+    
+    process {
+    }
+    
+    end {
+    }
+}
