f1d3c0478427d529719ace1b10104986f79c03ba
Jackbennett <jbennett@birkdalehigh.co.uk>
2019-03-07 14:36:05 +0000

    Throwing from get-schoolUser is broken ruining the tests

--- User.tests.ps1
+++ User.tests.ps1
@@ -4,64 +4,62 @@ import-module "$PSScriptRoot\SchoolGroups.psm1" -force
 # Example 2: https://hastebin.com/seresoseve.scala
 # Example 3: https://hastebin.com/ixebepagik.php
 
-Describe 'New-SchoolUser' {
-    BeforeAll {
-        $defaultParams = @{
-            GivenName      = 'First'
-            Surname        = 'Last'
-            EmployeeNumber = '001234'
-            Intake         = '2018'
-        }
-        Mock ValidateIntake -ModuleName SchoolGroups { $true }
-        Mock New-HomeDirectory -ModuleName SchoolGroups
-        Mock Get-ADUser -ModuleName SchoolGroups {
-            if($script:NewADUser){
-                return $script:NewADUser
+InModuleScope SchoolGroups {
+    Describe 'New-SchoolUser' {
+        Mock ValidateIntake {return $true}
+        Context "Creation"{
+            Mock Get-SchoolUser {
+                Throw [Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException]::new("User not found")
             }
-        }
-        Mock New-ADUser -ModuleName SchoolGroups {
-            $u = [Microsoft.ActiveDirectory.Management.ADUser]::new()
-            $u.SamAccountName = $name
-            $u.HomeDirectory = '\\example\path'
-            $u.EmployeeNumber = $EmployeeNumber
+            Mock Get-SchoolUser { # Existing employeeNumber
+                New-Object -TypeName Microsoft.ActiveDirectory.Management.ADUser -Property @{
+                    SamAccountName = $username
+                    EmployeeNumber = $EmployeeNumber
+                    HomeDirectory = 'Test Data'
+                }
+            } -ParameterFilter { $InputObject.EmployeeNumber -eq '002022' }
+            Mock New-ADUser {
+                New-Object -TypeName Microsoft.ActiveDirectory.Management.ADUser -Property @{
+                SamAccountName = $username
+                EmployeeNumber = $EmployeeNumber
+                HomeDirectory = 'Test Data'
+            }}
+            Mock New-HomeDirectory {}
+            Mock Get-ADUser {$script:NewUser}
 
-            return $script:NewADUser = $u
-        }
-    }
+            It "Create 1 user"{
+                $account = New-SchoolUser -Givenname "Tester" -Surname "First" -EmployeeNumber '001011' -intake 1910
 
+                Assert-MockCalled New-ADUser -Times 1 -Exactly -Scope 'It'
+                Assert-MockCalled New-HomeDirectory -Times 1 -Exactly -Scope 'It'
+                $account.SamAccountName | Should -be "10FirstT"
+            }
+            It "-NoHome parameter does not call New-HomeDriectory"{
+                $account = New-SchoolUser -NoHome -Givenname "Tester" -Surname "First" -EmployeeNumber '001011' -intake 1910
 
-    Context "Add 1 User, Ideal example"{
-        It "Create 1 user"{
-            $account = New-SchoolUser @defaultParams
+                Assert-MockCalled New-ADUser -Times 1 -Exactly -Scope 'It'
+                Assert-MockCalled New-HomeDirectory -Times 0 -Exactly -Scope 'It'
+                $account.SamAccountName | Should -be "10SecondT"
+            }
+            It "Does not create a duplicate employeeNumber"{
+                {New-SchoolUser -Givenname "Different" -Surname "User" -EmployeeNumber '002022' -intake 1910} | Should -Throw
 
-            Assert-MockCalled New-ADUser -Times 1 -Exactly -ModuleName SchoolGroups
-            Assert-MockCalled New-HomeDirectory -Times 1 -Exactly -ModuleName SchoolGroups
-            $account.SamAccountName | Should -be "18LastF"
-        }
-        It "Will not create a duplicate employeeNumber"{
-            $newParams = $defaultParams
-            $newParams.EmployeeNumber = '001233'
-            $account = New-SchoolUser @newParams
+                Assert-MockCalled Get-SchoolUser -Times 1 -Exactly -Scope 'It'
+                Assert-MockCalled New-ADUser -Times 0 -Exactly -Scope 'It'
+                Assert-MockCalled New-HomeDirectory -Times 0 -Exactly -Scope 'It'
+            }
+            It "Catch incorrect EmployeeID formats"{
+                {New-SchoolUser -Givenname "Different" -Surname "User" -EmployeeNumber '00101' -intake 1910} | Should -Throw
+                {New-SchoolUser -Givenname "Different" -Surname "User" -EmployeeNumber 'XY1101' -intake 1910} | Should -Throw
 
-            Assert-MockCalled New-ADUser -Times 1 -Exactly -ModuleName SchoolGroups
-            Assert-MockCalled New-HomeDirectory -Times 1 -Exactly -ModuleName SchoolGroups
-            $account.SamAccountName | Should -be "18LastF1"
-        }
-        It "Increment the username number for duplicates"{
-            # TODO
-        }
-    }
-    context "Do not create Home Directroy"{
-        It "nohome parameter does not call New-HomeDriectory"{
-            $newParams = $defaultParams
-            $newParams.noHome = $true
-            $newParams.GivenName = 'Second'
-            $newParams.EmployeeNumber = '001235'
-            $account = New-SchoolUser @newParams
+                Assert-MockCalled Get-SchoolUser -Times 0 -Exactly -Scope 'It'
+                Assert-MockCalled New-ADUser -Times 0 -Exactly -Scope 'It'
+                Assert-MockCalled New-HomeDirectory -Times 0 -Exactly -Scope 'It'
+            }
+            It "Increment the username number for duplicates"{
+                Throw "TODO"
+            }
 
-            Assert-MockCalled New-ADUser -Times 1 -Exactly -ModuleName SchoolGroups
-            Assert-MockCalled New-HomeDirectory -Times 0 -Exactly -ModuleName SchoolGroups
-            $account.SamAccountName | Should -be "18LastS"
         }
     }
 }
